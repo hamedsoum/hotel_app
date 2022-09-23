490548ed42c7654a94a6891b895aaf7e6ab6be25
hamedsoum <hamedsoum4023@gmail.com>
2022-09-23 00:57:23 +0000

    form created with formBuilder and validator handle with setValidator, clear validator and updateValueAndValidity

--- src/app/authentification/register-reactive-form/register-reactive-form.component.ts
+++ src/app/authentification/register-reactive-form/register-reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from '../register/user';
 
 @Component({
@@ -14,20 +14,51 @@ export class RegisterReactiveFormComponent implements OnInit {
 
   public user : User = new User();
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      firtName : new FormControl(),
-      lastName : new FormControl(),
-      email : new FormControl(),
-      sendCatalogue : new FormControl(true)
-    })
+    this.registerForm = this.fb.group({
+      firstName : ['',[Validators.required, Validators.maxLength(20)]],
+      // lastName : {value : 'indisponible', disabled : true},
+      lastName : ['',  [Validators.required, Validators.minLength(4)]],
+      email :[ '', [Validators.required, Validators.email]],
+      phone : '',
+      notification : 'email',
+      sendCatalogue : false
+
+    });
+    // this.registerForm = new FormGroup({
+    //   firstName : new FormControl(null, [Validators.required, Validators.maxLength(20)]),
+    //   lastName : new FormControl(null, [Validators.required, Validators.minLength(4)]),
+    //   email : new FormControl(null, [Validators.required, Validators.email]),
+    //   sendCatalogue : new FormControl(false)
+    // })
   }
 
   public saveData(){
+    
+  }
 
-        
+  public fillFormData(){
+    this.registerForm.setValue({
+      firstName : 'John',
+      lastName : 'Doe',
+      email: 'jdoe@test.com',
+      sendCatalogue : true
+    })
+  }
+
+  public setNotificationSetting(method : string): void{
+      const phoneControl = this.registerForm.get('phone');
+
+
+      if (method === 'text') {
+        phoneControl?.setValidators(Validators.required);
+      }else{
+        phoneControl?.clearValidators();
+      }
+
+      phoneControl?.updateValueAndValidity();
   }
 
 }
